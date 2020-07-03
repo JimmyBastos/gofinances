852f4e0078556ec145e9d4436c2da2e9d4859f43
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-07-02 21:57:34 -0300

    Updates

---
+++ precache-manifest.6809742de4cca33d9711d72ac9389856.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be5ba7887bd8f6b695df061bc2608cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f030bd215beffb33598b",
+    "url": "/static/js/2.e3212d03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e3212d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c5b46d3c73385e2712",
+    "url": "/static/js/main.dabb283b.chunk.js"
+  },
+  {
+    "revision": "9fdb30ff8f91e279e582",
+    "url": "/static/js/runtime-main.964e4442.js"
+  },
+  {
+    "revision": "78a343ae8fc9abcf44feb04b5656f5fc",
+    "url": "/static/media/alert.78a343ae.svg"
+  },
+  {
+    "revision": "df4567f063bf4458db532e5ac63df30a",
+    "url": "/static/media/income.df4567f0.svg"
+  },
+  {
+    "revision": "ce269cac7c70f224e69b361665e6d3e0",
+    "url": "/static/media/logo.ce269cac.svg"
+  },
+  {
+    "revision": "bb31a5f33ccfe334fa7c72e294ba2984",
+    "url": "/static/media/outcome.bb31a5f3.svg"
+  },
+  {
+    "revision": "7f99c5ac38306dc0adf301bf383f882f",
+    "url": "/static/media/total.7f99c5ac.svg"
+  }
+]);
